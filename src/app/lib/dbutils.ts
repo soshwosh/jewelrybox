0b85efc38833437c0af89f5f1d6db5ba2440d67e
soshwosh <sasharavpersaud@gmail.com>
2025-01-01 13:34:22 -0500

    wrote dbutil function to fetch all jewelry items

--- src/app/lib/dbutils.ts
+++ src/app/lib/dbutils.ts
@@ -1,1 +1,13 @@
 import {sql} from '@vercel/postgres'
+import { SelectJewelryItem } from '../types/drizzleTypes'
+
+export async function fetchAllJewelryItems() {
+    try {
+        const data = await sql<SelectJewelryItem>`SELECT * FROM revenue`;
+        return data.rows;
+    }
+    catch(e) {
+        console.error("Database Error: ", e);
+        throw new Error("Failed to fetch jewelry item data.")
+    }
+}
