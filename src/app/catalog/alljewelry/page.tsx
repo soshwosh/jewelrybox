e29374e9ed8c9d5d49e770d07e4837cdb004cdcf
soshwosh <sasharavpersaud@gmail.com>
2025-01-01 11:39:34 -0500

    rendered roughly styled cards with dummy data

--- src/app/catalog/alljewelry/page.tsx
+++ src/app/catalog/alljewelry/page.tsx
@@ -1,3 +1,30 @@
+import CatalogGallery from "@/app/ui/cataloggallery/cataloggallery";
+// import a db util function to fetch al jewelry items
+
 export default function Page() {
-  return <h1 className="text-6xl">All Jewelry</h1>;
+  // call db util function, store return obj of all jewelry items in a variable
+  const dummyData = [
+    {id: 1,
+    userid: 1,
+    name: "organized chaos choker",
+    type: "necklace",
+    material: "gold",
+    color: "multcolor",
+    brand: "",
+    notes: "",
+    imageUrl: "",},
+    {id: 2,
+    userid: 2,
+    name: "organized chaos choker",
+    type: "necklace",
+    material: "gold",
+    color: "multcolor",
+    brand: "",
+    notes: "",
+    imageUrl: "",},
+
+  ];
+  return (
+    <CatalogGallery jewelryList={dummyData}/> 
+  );
 }
---
+++ src/app/types/jewelryItemType.ts
@@ -0,0 +1,11 @@
+export type JewelryItemType = {
+    id: number,
+    userid: number,
+    name: string,
+    type: string,
+    material: string,
+    color: string,
+    brand: string,
+    notes: string,
+    imageUrl: string,
+}
