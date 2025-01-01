22ff88b1b32d34d3e8ff4fe78f315bec02d6032c
soshwosh <sasharavpersaud@gmail.com>
2025-01-01 15:50:30 -0500

    styled home page for easier navigation while testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <div className="flex flex-col justify-center items-center h-screen bg-pink-300">
+      <h1 className="text-5xl text-white">Jewelry Box</h1>
+      <div className="flex flex-col m-5 w-1/6 bg-pink-500 p-4 rounded-lg">
+        <button
+          className="w-auto bg-pink-700 px-3 py-1 rounded-md text-white text-xl m-2 hover:bg-pink-200 hover:text-black"
+          type="submit"
+        >
+          <Link href="/catalog/alljewelry">Log In</Link>
+        </button>
+        <button
+          className="bg-pink-700 px-3 py-1 rounded-md text-white text-xl m-2 hover:bg-pink-200 hover:text-black"
+          type="submit"
+        >
+          <Link href="/catalog/alljewelry">Sign Up</Link>
+        </button>
+      </div>
+    </div>
   );
 }
