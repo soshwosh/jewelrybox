f41f079e291135180c98adb5dd2572095da3207e
soshwosh <sasharavpersaud@gmail.com>
2025-01-01 08:40:57 -0500

    stubbed out some pages

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,4 @@
+
+export default async function Page() {
+  return <h1>Catalog</h1>;
+}
---
+++ src/app/catalog/rings/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1 className="text-6xl">Rings</h1>
+    );
+}
