991bd4e232a526b353bb980c844cf4cbf14aa13f
soshwosh <sasharavpersaud@gmail.com>
2025-01-02 13:19:03 -0500

    added some responsive sizing for mobile devices to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-pink-300">
       <h1 className="text-5xl text-white">Jewelry Box</h1>
-      <div className="flex flex-col m-5 w-1/6 bg-pink-500 p-4 rounded-lg">
+      <div className="flex flex-col m-5 w-1/2 sm:w-1/3 md:w-1/6 bg-pink-500 p-4 rounded-lg">
         <button
           className="w-auto bg-pink-700 px-3 py-1 rounded-md text-white text-xl m-2 hover:bg-pink-200 hover:text-black"
           type="submit"
