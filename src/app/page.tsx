16e16067355572faf466dbd72302e526556e9e5c
soshwosh <sasharavpersaud@gmail.com>
2025-01-06 18:10:17 -0500

    removed button parent to login & signup Links on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,20 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center h-screen bg-pink-300">
       <h1 className="text-5xl text-white">Jewelry Box</h1>
       <div className="flex flex-col m-5 w-1/2 sm:w-1/3 md:w-1/6 bg-pink-500 p-4 rounded-lg">
-        <button
-          className="w-auto bg-pink-700 px-3 py-1 rounded-md text-white text-xl m-2 hover:bg-pink-200 hover:text-black"
-          type="submit"
+        <Link
+          key="/alljewelry"
+          href="/alljewelry"
+          className="w-auto text-center bg-pink-700 px-3 py-1 rounded-md text-white text-xl m-2 hover:bg-pink-200 hover:text-black"
         >
-          <Link href="/alljewelry">Log In</Link>
-        </button>
-        <button
-          className="bg-pink-700 px-3 py-1 rounded-md text-white text-xl m-2 hover:bg-pink-200 hover:text-black"
-          type="submit"
+          Log In
+        </Link>
+        <Link
+          key="/alljewelry"
+          href="/alljewelry"
+          className="w-auto text-center bg-pink-700 px-3 py-1 rounded-md text-white text-xl m-2 hover:bg-pink-200 hover:text-black"
         >
-          <Link href="/alljewelry">Sign Up</Link>
-        </button>
+          Sign In
+        </Link>
       </div>
     </div>
   );
