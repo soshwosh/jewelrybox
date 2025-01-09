dbabb79eaa82560ff6ab6567c560d98e8ac28bce
soshwosh <sasharavpersaud@gmail.com>
2025-01-09 10:41:51 -0500

    captured input from search bar and setting as query param

--- src/app/ui/cataloggallery/searchbar.tsx
+++ src/app/ui/cataloggallery/searchbar.tsx
@@ -1,4 +1,20 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
 export default function SearchBar({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+
+  function handleSearch(input: string) {
+    const params = new URLSearchParams(searchParams);
+    
+    if (input) {
+      params.set("query", input);
+    } else {
+      params.delete("query");
+    }
+    console.log(params.get("query"));
+  }
+
   return (
     <div className="flex justify-center">
       <input
@@ -7,6 +23,7 @@ export default function SearchBar({ placeholder }: { placeholder: string }) {
         id=""
         placeholder={placeholder}
         className="bg-slate-100 w-full md:w-1/2 rounded-2xl px-3 py-2 my-5 outline-none"
+        onChange={(e) => handleSearch(e.target.value)}
       />
     </div>
   );
