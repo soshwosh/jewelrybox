284281916c043cf0ddac071de84c45104a888d7f
soshwosh <sasharavpersaud@gmail.com>
2025-01-08 19:56:08 -0500

    added responsive styling to search bar and add item form

--- src/app/ui/cataloggallery/searchbar.tsx
+++ src/app/ui/cataloggallery/searchbar.tsx
@@ -1,11 +1,13 @@
 export default function SearchBar() {
     return (
-      <input
-        type="search"
-        name=""
-        id=""
-        placeholder="Search jewelry catalog"
-        className="bg-slate-100 w-full rounded-2xl px-3 py-2 my-2 outline-none"
-      />
+      <div className="m-3 px-3 w-full">
+        <input
+          type="search"
+          name=""
+          id=""
+          placeholder="Search jewelry catalog"
+          className="bg-slate-100 w-full rounded-2xl px-3 py-2 my-2 outline-none"
+        />
+      </div>
     );
 }
